b76fd346a7eadca020557acbdaac0c7227fd3950
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-12-02 10:21:28 +0000

    Added day two part one

---
+++ day_two/password_part_one.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+
+def sanitize_passwords(passwords):
+    return passwords.strip()
+
+
+def sanitize_validation(validators):
+    """sanitize validation in the form xn-yn z"""
+    joined_bounds, letter = validators.split(" ")
+    lower_bound, upper_bound = map(int, joined_bounds.split("-"))
+    return lower_bound, upper_bound, letter
+
+
+with open("passwords.txt") as passwords:
+    valid_passwords = 0
+    for line in passwords.readlines():
+        validation_rules, password_to_check = line.strip().split(":")
+        lower_bound, upper_bound, letter = sanitize_validation(
+            validation_rules)
+        password = sanitize_passwords(password_to_check)
+        letter_counter = Counter(password)
+        if lower_bound <= letter_counter.get(letter, 0) <= upper_bound:
+            valid_passwords += 1
+    print(valid_passwords)
