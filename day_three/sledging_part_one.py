bb69585a2e37b8305ea809174834ae43204f56e5
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-12-03 17:14:12 +0000

    Refactor for DRY'ness

--- day_three/sledging_part_one.py
+++ day_three/sledging_part_one.py
@@ -1,14 +1,20 @@
-world_map = []
-with open("map.txt") as raw_world_map:
-    world_map = raw_world_map.read().splitlines()
+def ingest_file():
+    with open("map.txt") as raw_world_map:
+        return raw_world_map.read().splitlines()
 
-trees = 0
-row, column = 0, 0
-while row + 1 < len(world_map):
-    row += 1
-    column += 3
 
-    location = world_map[row][column % len(world_map[row])]
-    if location == "#":
-        trees += 1
-print(trees)
+def traverse_world(world_map, row_adjustment=1, column_adjustment=3):
+    trees = 0
+    row, column = 0, 0
+    while row + 1 < len(world_map):
+        row += row_adjustment
+        column += column_adjustment
+        location = world_map[row][column % len(world_map[row])]
+        if location == "#":
+            trees += 1
+    return trees
+
+
+if __name__ == "__main__":
+    world_map = ingest_file()
+    print(traverse_world(world_map))
--- day_three/sledging_part_two.py
+++ day_three/sledging_part_two.py
@@ -1,20 +1,8 @@
-world_map = []
-with open("map.txt") as raw_world_map:
-    world_map = raw_world_map.read().splitlines()
+from sledging_part_one import ingest_file, traverse_world
+from math import prod
 
-
-movements = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
-
-total = 1
-for move in movements:
-    trees = 0
-    row, column = 0, 0
-    while row + 1 < len(world_map):
-        row += move[1]
-        column += move[0]
-
-        location = world_map[row][column % len(world_map[row])]
-        if location == "#":
-            trees += 1
-    total *= trees
-print(total)
+if __name__ == "__main__":
+    world_map = ingest_file()
+    movements = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    print(prod(traverse_world(world_map, row_adjustment=column,
+                              column_adjustment=row) for row, column in movements))
