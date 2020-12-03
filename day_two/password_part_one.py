6e31df76ef8d09c3ccf25c2ca177efa9853a1a96
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-12-03 08:18:31 +0000

    Refactor code to run as script

--- day_two/password_part_one.py
+++ day_two/password_part_one.py
@@ -12,14 +12,15 @@ def sanitize_validation(validators):
     return lower_bound, upper_bound, letter
 
 
-with open("passwords.txt") as passwords:
-    valid_passwords = 0
-    for line in passwords.readlines():
-        validation_rules, password_to_check = line.strip().split(":")
-        lower_bound, upper_bound, letter = sanitize_validation(
-            validation_rules)
-        password = sanitize_passwords(password_to_check)
-        letter_counter = Counter(password)
-        if lower_bound <= letter_counter.get(letter, 0) <= upper_bound:
-            valid_passwords += 1
-    print(valid_passwords)
+if __name__ == "__main__":
+    with open("passwords.txt") as passwords:
+        valid_passwords = 0
+        for line in passwords.readlines():
+            validation_rules, password_to_check = line.strip().split(":")
+            lower_bound, upper_bound, letter = sanitize_validation(
+                validation_rules)
+            password = sanitize_passwords(password_to_check)
+            letter_counter = Counter(password)
+            if lower_bound <= letter_counter.get(letter, 0) <= upper_bound:
+                valid_passwords += 1
+        print(valid_passwords)
