e1608aa18fa01d9e772236714e42c91a27429782
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-12-07 09:01:35 +0000

    Refactor passports

--- day_four/passports_part_one.py
+++ day_four/passports_part_one.py
@@ -1,6 +1,24 @@
 fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
 
 
+def ingest_file():
+    with open("passports.txt") as passports:
+        return [line.strip() for line in passports.readlines()]
+
+
+def generate_passport_keys(raw_data):
+    passport_keys = list()
+    current_passport = ""
+    for line in raw_data:
+        current_passport += line
+        if line == "":
+            passport_keys.append(current_passport)
+            current_passport = ""
+    # ingest last line
+    passport_keys.append(current_passport)
+    return passport_keys
+
+
 def is_passport_valid(pp):
     for field in fields:
         if field not in pp:
@@ -8,20 +26,8 @@ def is_passport_valid(pp):
     return True
 
 
-with open("passports.txt") as passports:
-    data = passports.readlines()
-    data = [line.strip() for line in data]
-
-valid_count = 0
-current_passport = ""
-for line in data:
-    if line != '':
-        current_passport += ' ' + line
-    else:
-        print(current_passport)
-        if is_passport_valid(current_passport):
-            valid_count += 1
-        current_passport = ""
-if is_passport_valid(current_passport):
-    valid_count += 1
-print(valid_count)
+if __name__ == "__main__":
+    data = ingest_file()
+    valid_count = 0
+    passport_keys = generate_passport_keys(data)
+    print(sum(1 for key in passport_keys if is_passport_valid(key)))
